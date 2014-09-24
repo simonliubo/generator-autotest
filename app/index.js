e422a6915c470d2f9210fd8d58f0fe3046b4165f
liubo <liubo@beisen.com>
2014-09-24 14:51:07 +0800

    init

--- app/index.js
+++ app/index.js
@@ -9,27 +9,27 @@ var AutotestGenerator = yeoman.generators.Base.extend({
     this.pkg = require('../package.json');
   },
 
-  prompting: function () {
-    var done = this.async();
-
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the beautiful Autotest generator!'
-    ));
-
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
-
-      done();
-    }.bind(this));
-  },
+//  prompting: function () {
+//    var done = this.async();
+//
+//    // Have Yeoman greet the user.
+//    this.log(yosay(
+//      'Welcome to the beautiful Autotest generator!'
+//    ));
+//
+//    var prompts = [{
+//      type: 'confirm',
+//      name: 'someOption',
+//      message: 'Would you like to enable this option?',
+//      default: true
+//    }];
+//
+//    this.prompt(prompts, function (props) {
+//      this.someOption = props.someOption;
+//
+//      done();
+//    }.bind(this));
+//  },
 
   writing: {
     app: function () {
