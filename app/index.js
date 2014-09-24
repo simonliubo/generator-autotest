6091a89aa49d25f061ba11b7f1363534a75d13c6
liubo <liubo@beisen.com>
2014-09-24 12:27:20 +0800

    init

--- app/index.js
+++ app/index.js
@@ -1,3 +1,54 @@
-/**
- * Created by liubo on 14-9-24.
- */
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var yosay = require('yosay');
+
+var AutotestGenerator = yeoman.generators.Base.extend({
+  initializing: function () {
+    this.pkg = require('../package.json');
+  },
+
+  prompting: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the beautiful Autotest generator!'
+    ));
+
+    var prompts = [{
+      type: 'confirm',
+      name: 'someOption',
+      message: 'Would you like to enable this option?',
+      default: true
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.someOption = props.someOption;
+
+      done();
+    }.bind(this));
+  },
+
+  writing: {
+    app: function () {
+      this.dest.mkdir('app');
+      this.dest.mkdir('app/templates');
+
+      this.src.copy('_package.json', 'package.json');
+      this.src.copy('_bower.json', 'bower.json');
+    },
+
+    projectfiles: function () {
+      this.src.copy('editorconfig', '.editorconfig');
+      this.src.copy('jshintrc', '.jshintrc');
+    }
+  },
+
+  end: function () {
+    this.installDependencies();
+  }
+});
+
+module.exports = AutotestGenerator;
