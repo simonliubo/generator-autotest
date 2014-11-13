6bd3168825664ea484202a388608a58440bd5a3c
liubo <liubo@beisen.com>
2014-11-13 14:32:18 +0800

    add golbals.js

---
+++ app/templates/globals.js
@@ -0,0 +1,62 @@
+/**
+ * Created by liubo on 14/11/13.
+ */
+var underscore = require("./app/components/underscore/underscore.js");
+module.exports = {
+    "_": underscore
+    ,"localhost" : {
+        "loginname": "wang_hui_er@163.com"
+        ,"password": "111111"
+        ,"username": "wanghui"
+        ,"email": "wang_hui_er@163.com"
+
+    }
+    ,"xp-firefox-liubo" : {
+        "loginname": "wang_hui_er@163.com"
+        ,"password": "111111"
+        ,"username": "wanghui"
+        ,"email": "wang_hui_er@163.com"
+    }
+    ,"xp-chrome-liubo" : {
+        "loginname": "wang_hui_er@163.com"
+        ,"password": "111111"
+        ,"username": "wanghui"
+        ,"email": "wang_hui_er@163.com"
+    }
+    ,"xp-firefox-wanghui" : {
+        "loginname": "wang_hui_er@163.com"
+        ,"password": "111111"
+        ,"username": "wanghui"
+        ,"email": "wang_hui_er@163.com"
+    }
+    ,"xp-chrome-wanghui" : {
+        "loginname": "wang_hui_er@163.com"
+        ,"password": "111111"
+        ,"username": "wanghui"
+        ,"email": "wang_hui_er@163.com"
+    }
+    ,"win7-chrome-wanghui" : {
+        "loginname": "wang_hui_er@163.com"
+        ,"password": "111111"
+        ,"username": "wanghui"
+        ,"email": "wang_hui_er@163.com"
+    }
+    ,"win7-chrome381-liubo" : {
+        "loginname": "wang_hui_er@163.com"
+        ,"password": "111111"
+        ,"username": "wanghui"
+        ,"email": "wang_hui_er@163.com"
+    }
+    ,"win7-chrome382-liubo" : {
+        "loginname": "wang_hui_er@163.com"
+        ,"password": "111111"
+        ,"username": "wanghui"
+        ,"email": "wang_hui_er@163.com"
+    }
+    ,"win7-ie-liubo" : {
+        "loginname": "wang_hui_er@163.com"
+        ,"password": "111111"
+        ,"username": "wanghui"
+        ,"email": "wang_hui_er@163.com"
+    }
+}
